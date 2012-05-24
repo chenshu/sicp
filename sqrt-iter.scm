617bec78f50e1e7591e533eef3581e61dd784b06
chen shu <chenshu510@gmail.com>
2012-05-24 10:23:04 +0800

    牛顿法求平方根

---
+++ sqrt-iter.scm
@@ -0,0 +1,35 @@
+;guess = sqrt(x)
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x)
+               x)))
+
+(define (improve guess x)
+  (average guess
+           (/ x guess)))
+
+(define (average x y)
+  (/ (+ x
+        y)
+     2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess)
+             x))
+     0.001))
+
+(define (sqrt-iter2 guess x)
+  (if (< (abs (- (square guess)
+                 x))
+         0.001)
+    guess
+    (sqrt-iter2 (/ (+ guess
+                      (/ x guess))
+                   2)
+                x)))
+
+(sqrt-iter 1 2)
+(sqrt-iter 1 10)
+(sqrt-iter2 1 2)
+(sqrt-iter2 1 10)
